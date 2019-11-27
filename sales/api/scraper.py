c4809208a263fc233bfcb6cc941bb0d5992a7909
Matt Kong <mattkong@umich.edu>
2019-11-27 11:39:18 -0500

    Return product name and price from web scrape.

--- sales/api/scraper.py
+++ sales/api/scraper.py
@@ -5,14 +5,17 @@ from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-logger = logging.getLogger(__name__)
+# from .models import Item
+
+LOGGER = logging.getLogger(__name__)
+BLANK = ' '
 
 def get_brand(url):
     parsed = urlparse(url)
     if parsed.netloc:
         return parsed.netloc.split('.')[1]
     else:
-        logger.error('Invalid URL: Unable to find brand.\nCopy link directly from browser.')
+        LOGGER.error('Invalid URL: Unable to find brand.\nCopy link directly from browser.')
 
 def scrape(url):
     brand = get_brand(url) # TODO: Notify client of invalid URL
@@ -27,10 +30,24 @@ def scrape(url):
     page = BeautifulSoup(html, 'html.parser')
 
     if brand == 'jcrew':
-        logger.info('Scraping J.Crew page...')
-        print("Looking at {}".format(page.title.string))
-        reg_price = page.find_all(class_='is-price')[0]
-        print("Price: {}".format(reg_price.string))
+        LOGGER.info('Scraping J.Crew page...')
+        product = page.find_all(class_='product__name')[0].string
+        print("Looking at", product)
+        if not product:
+            LOGGER.error('Invalid item: no product name found')
+
+        reg_price = page.find_all(class_='is-price')[0].string
+        sale_price = page.find_all(class_='sale-price')[0].string
+        price = sale_price if sale_price != BLANK else reg_price
+        price = float(price.strip('$'))
+
+        print("Price: ", price)
+        if not price:
+            LOGGER.error('Invalid item: no pricing found')
+
+        return product, price
+
+        
 
 
 
--- sales/api/test/test_scraper.py
+++ sales/api/test/test_scraper.py
@@ -13,5 +13,7 @@ def test_get_brand_invalid():
 
 def test_scrape_jcrew():
     url = 'https://www.jcrew.com/p/mens_category/coats_and_jackets/bomber_jacket/harrington-jacket/H6538?color_name=warm-indigo'
-    scraper.scrape(url)
+    product, price = scraper.scrape(url)
+    assert product == "Harrington jacket"
+    assert price == 178.00
 
